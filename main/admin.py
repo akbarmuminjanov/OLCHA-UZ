5711864028814a30351c10ff1b56e32e9da261b7
akbarmuminjanov <muminjanov.75@gmail.com>
2024-04-22 20:37:23 +0500

    commit

--- main/admin.py
+++ main/admin.py
@@ -1,15 +1,18 @@
 from django.contrib import admin
-from .models import Category, Product, ProductImage
+from .models import Category, Product, ProductImage, Specifications
 # Register your models here.
 class ProductImageInline(admin.TabularInline):
     model = ProductImage
+
+class SpecificationsInline(admin.TabularInline):
+    model = Specifications
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ["name", "product_kod", "category"]
     search_fields = ["product_kod", 'category']
     prepopulated_fields = {'slug': ('name',)}
-    inlines = [ProductImageInline]
+    inlines = [ProductImageInline, SpecificationsInline]
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
--- main/models.py
+++ main/models.py
@@ -44,3 +44,7 @@ class Product(models.Model):
 class ProductImage(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     image = models.ImageField(upload_to="mahsulot")
+
+class Specifications(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    specifications = models.CharField(max_length=255)
